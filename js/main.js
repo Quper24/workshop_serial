d7dd2dacafd9169f378edb90e0bb18d362242e8a
Quper24 <Quper2405@gmail.com>
2020-05-21 07:19:37 +0500

    addd card api

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-const API_KEY = '4e61d32c7f8095da04f6550d8cc3dd94';
 const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const API_KEY = '4e61d32c7f8095da04f6550d8cc3dd94';
+const leftMenu = document.querySelector(".left-menu");
+hamburger = document.querySelector(".hamburger"),
+    cardsList = document.querySelector('.tv-shows__list'),
+    modal = document.querySelector('.modal'),
+    img = modal.querySelector('.tv-card__img'),
+    link = modal.querySelector('.modal__link'),
+    title = modal.querySelector('.modal__title'),
+    genres = modal.querySelector('.genres-list'),
+    rating = modal.querySelector('.rating'),
+    description = modal.querySelector('.description'),
+    tvShows = document.querySelector('.tv-shows'),
+    preloader = document.querySelector('.preloader'),
+    searchForm = document.querySelector('.search__form'),
+    searchFormInput = document.querySelector('.search__form-input');
+
+
+const loading = document.createElement('div');
+loading.className = 'loading';
+
 
 class DBService {
-
     getResource = async (url) => {
         const res = await fetch(url);
         if (!res.ok) {
@@ -18,134 +36,138 @@ class DBService {
     getTestCard = async () => {
         return await this.getResource('card.json');
     };
+
+    getSearchResult = async (query) => {
+        return await this.getResource('https://api.themoviedb.org/3/search/tv?api_key=' +
+            API_KEY +
+            '&language=ru-RU&page=1&include_adult=false&query=' +
+            query);
+    };
+
 }
 
-let searchForm = document.querySelector('.search__form');
-let cardsList = document.querySelector('.tv-shows__list');
+const renderCards = response => {
+    console.log(response);
+    cardsList.textContent = '';
+    response.results.forEach(item => {
+        const {
+            backdrop_path: backdrop,
+            name: title,
+            poster_path: poster,
+            vote_average: vote
+        } = item;
 
-const renderSearch = () => {
-    new DBService().getTestCards().then((response) => {
-        console.log(response);
-        cardsList.textContent = '';
-        response.results.forEach(({backdrop_path: backdrop, name, poster_path: poster, vote_average: vote}) => {
-            let posterIMG = poster ? IMG_URL + poster : './img/no-poster.jpg';
-            let backdropIMG = backdrop ? IMG_URL + backdrop : '';
-            const card = document.createElement('li');
-            card.className = 'tv-shows__item'
-            card.innerHTML = `
-					<a href="#" class="tv-card">
-							${vote ? `<span class="tv-card__vote">${vote}</span>` : ''}
-							<img class="tv-card__img" src="${posterIMG}" data-backdrop="${backdropIMG}" alt="${name}">
-							<h3 class="tv-card__head">${name}</h3>
-					</a>
-			`;
-            cardsList.insertAdjacentElement('beforeend', card);
-        });
+        const posterIMG = poster ? IMG_URL + poster : './img/no-poster.jpg';
+        const backdropIMG = backdrop ? IMG_URL + backdrop : '';
+        const voteValue = vote ? `<span class="tv-card__vote">${vote}</span>` : '';
+
+        const card = document.createElement('li');
+        card.className = 'tv-shows__item'
+        card.innerHTML = `
+			<a href="#" class="tv-card">
+				${voteValue}
+				<img class="tv-card__img"
+					src="${posterIMG}"
+					data-backdrop="${backdropIMG}"
+					alt="${title}">
+				<h4 class="tv-card__head">${title}</h4>
+			</a>
+		`;
+        loading.remove();
+        cardsList.insertAdjacentElement('beforeend', card);
     });
 };
 
-renderSearch();
-
-
-//menu
-const leftMenu = document.querySelector(".left-menu");
-const hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener('click', function () {
-
-    leftMenu.classList.toggle("openMenu");
-    hamburger.classList.toggle("open");
-
-
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const value = searchFormInput.value;
+    searchFormInput.style.borderTopColor = '';
+    if (value.trim()) {
+        tvShows.append(loading);
+        new DBService().getSearchResult(value).then(renderCards);
+    } else {
+        searchFormInput.style.borderTopColor = 'red';
+    }
+    return false
 });
 
-leftMenu.addEventListener('click', (event) => {
+
+
+// 1) открытие/закрытие на кнопку
+hamburger.addEventListener('click', () => {
+    leftMenu.classList.toggle("openMenu");
+    hamburger.classList.toggle("open");
+});
+
+// 2) закрытие по клику мимо меню
+document.body.addEventListener('click', event => {
+    if (!event.target.closest('.left-menu')) {
+        leftMenu.classList.remove("openMenu");
+        hamburger.classList.remove("open");
+    }
+});
+
+// 3) раскрытие dropmenu
+leftMenu.addEventListener('click', event => {
     const target = event.target;
     const dropdown = target.closest('.dropdown');
     if (dropdown) {
         dropdown.classList.toggle('active');
+        // 4) открывать меню при клике на иконки
         leftMenu.classList.add("openMenu");
         hamburger.classList.add("open");
     }
-
-
 });
 
-//смена карточки
 
+//модальное окно
 
-const changeImage = event => {
-    const target = event.target;
-    if (target.matches('.tv-card__img')) {
-        const changeImage = target.dataset.backdrop;
-        target.dataset.backdrop = target.src;
-        if (changeImage) {
-            target.src = changeImage
-        }
-    }
-};
-
-cardsList.addEventListener('mouseover', changeImage);
-cardsList.addEventListener('mouseout', changeImage);
-
-
-// модалка
-
-const modal = document.querySelector('.modal'),
-	img = modal.querySelector('.tv-card__img'),
-	link = modal.querySelector('.modal__link'),
-	title = modal.querySelector('.modal__title'),
-	genres = modal.querySelector('.genres-list'),
-	rating = modal.querySelector('.rating'),
-	description = modal.querySelector('.description');
-
+//открытие
 cardsList.addEventListener('click', event => {
     const target = event.target.closest('.tv-card');
 
     if (target) {
-
+        preloader.style.display = 'block';
         new DBService().getTestCard()
             .then(response => {
-				console.log(response)
-				img.src = IMG_URL + response.poster_path;
-				img.alt = response.name;
-				link.href = response.homepage;
-				title.textContent = response.name;
-				genres.textContent = '';
-				response.genres.forEach(item => {
-					genres.innerHTML += `<li>${item.name}</li>`
-				})
-				rating.textContent = response.vote_average;
-				description.textContent = response.overview;
+                img.src = IMG_URL + response.poster_path;
+                img.alt = response.name;
+                link.href = response.homepage;
+                title.textContent = response.name;
+                genres.textContent = '';
+                response.genres.forEach(item => {
+                    genres.innerHTML += `<li>${item.name}</li>`
+                });
+                rating.textContent = response.vote_average;
+                description.textContent = response.overview;
             })
             .then(() => {
-                modal.classList.remove('hide')
+                document.body.style.overflow = 'hidden';
+                modal.classList.remove('hide');
+            })
+            .then(() => {
+                preloader.style.display = 'none';
             })
     }
-
 });
 
 
-
-;
-
-
-//получить жанр
-
-// меню
-// https://developers.themoviedb.org/3/tv/get-top-rated-tv - топ рейтинг
-// https://developers.themoviedb.org/3/tv/get-popular-tv-shows - популярные
-
-// https://developers.themoviedb.org/3/tv/get-tv-airing-today - что показываю сегодня
-// https://developers.themoviedb.org/3/tv/get-tv-on-the-air - что будет на этой недели
+//закрытие
+modal.addEventListener('click', event => {
+    if (event.target.classList.contains('modal') ||
+        event.target.classList.contains('cross')) {
+        modal.classList.add('hide');
+        document.body.style.overflow = '';
+    }
+});
 
 
-// карточка сериала
-// https://developers.themoviedb.org/3/tv/get-tv-details - полное описание с сезонами
-//  https://developers.themoviedb.org/3/tv/get-tv-recommendations - рекомендации
-// https://developers.themoviedb.org/3/tv/get-similar-tv-shows - похожие
-// https://developers.themoviedb.org/3/tv/get-tv-videos - видео
-// https://developers.themoviedb.org/3/tv-seasons/get-tv-season-details - описание сезона
-
-
-
+//смена карточки
+const changeImage = event => {
+    const target = event.target;
+    if (target.matches('.tv-card__img') && target.dataset.backdrop) {
+        [target.dataset.backdrop, target.src] = [target.src, target.dataset.backdrop]
+    }
+};
+cardsList.addEventListener('mouseover', changeImage);
+cardsList.addEventListener('mouseout', changeImage);
